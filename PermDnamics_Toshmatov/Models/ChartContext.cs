2ad05b23747f1d129a3f757646a8ac7a1d58028b
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-04-06 11:28:45 +0500

    Установление пакетов NuGet, а также создание классы для БД и добавление кнопки на страницы Chart. Тошматов

---
+++ PermDnamics_Toshmatov/Models/ChartContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace PermDnamics_Toshmatov.Models
+{
+    public class ChartContext : DbContext
+    {
+        public DbSet<ChartSave> ChartSaves { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=charts.db");
+    }
+
+    public class ChartSave
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Data { get; set; }  
+        public DateTime SaveDate { get; set; }
+    }
+}
